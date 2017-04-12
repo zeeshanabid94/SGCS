dcf8fdd72de85427e6d8999fa7db1b9bce529110
zeeshanabid94 <zeeshanabid94@gmail.com>
2017-04-12 15:55:35 -0700

    Progress on BFSeparation

--- src/Frame.java
+++ src/Frame.java
@@ -2,39 +2,70 @@ import java.awt.image.BufferedImage;
 
 public class Frame {
 	MacroBlock[] _macroblocks;
-	int[] imageBytes;
+	BufferedImage image;
+	int width;
+	int height;
+	int macroblockSize = 16;
+	int wPadding;
+	int hPadding;
 	public Frame() {
-		imageBytes = null;
+		image = null;
 		_macroblocks = new MacroBlock[2025];
 	}
 	  
-	public Frame(int width, int height) {
-		imageBytes = new int[width*height*3];
-		_macroblocks = new MacroBlock[2025];
-
-		int x = 0;
-		int y = 0;
-
-		for (int i = 0; i < 2025; i++) {
-			_macroblocks[i] = new MacroBlock(imageBytes, x, y);
-			x+= 16;
-			if (x >= Video.WIDTH) {
-				x = 0;
-				y += 16;
-			}
-		}
+	public Frame(int w, int h, int wpadding, int hpadding) {
+		width = w;
+		height = h;
+		wPadding = wpadding;
+		hPadding = hpadding;
+		image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		int hMacro = height/16;
+		int wMacro = width/16;
+		_macroblocks = new MacroBlock[hMacro * wMacro];
+//
+//		int x = 0;
+//		int y = 0;
+//		
+//		for (int i = 0; i < _macroblocks.length; i++) {
+//			int[] pixels = new int[macroblockSize*macroblockSize];
+//			int ptrx = x;
+//			int ptry = y;
+//			for (int j = 0; j < macroblockSize*macroblockSize; j++) {
+//				pixels[i] = imageBytes[ptrx+ptry*height];
+//				ptrx++;
+//				if (ptrx >= x+16) {
+//					ptry++;
+//					ptrx = x;
+//				}
+//			}
+//			_macroblocks[i] = new MacroBlock(imageBytes, x, y);
+//			x+= 16;
+//			if (x >= width) {
+//				x = 0;
+//				y += 16;
+//			}
+//		}
 	}
 	
-	public int[] getBytes() {
-		return imageBytes;
+	public void setPixel(int x, int y, int pixel) {
+		x += wPadding/2;
+		y += hPadding/2;
+		image.setRGB(x, y, pixel);
 	}
+
 	
 	public BufferedImage getFrameImage() {
-		BufferedImage frame = new BufferedImage(Video.WIDTH, Video.HEIGHT, BufferedImage.TYPE_INT_RGB);
+		return image;
+	}
+	
+	public BufferedImage constructImageFromMacroblocks() {
+		BufferedImage frame = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 		
-		for (int i = 0; i < Video.HEIGHT * Video.WIDTH; i++) {
-			int pixel = 0x00000000 | ( imageBytes[i] << 16) | (imageBytes[i + Video.HEIGHT * Video.WIDTH] << 8) | (imageBytes[i + Video.HEIGHT * Video.WIDTH * 2]);
-			frame.setRGB(i%Video.WIDTH, i/Video.WIDTH, pixel);
+		for (MacroBlock block : _macroblocks) {
+			for (int i = 0; i < macroblockSize * macroblockSize; i++) {
+				int pixel = 0x00000000 | ( block.getPixels()[i] << 16) | (block.getPixels()[i + height * width] << 8) | (block.getPixels()[i + height * width * 2]);
+				frame.setRGB((block.getX()+i)%width, (block.getY()+i)/width, pixel);
+			}
 		}
 		
 		return frame;
--- src/Video.java
+++ src/Video.java
@@ -5,35 +5,105 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class Video {
-	  static int WIDTH = 960;
-	  static int HEIGHT = 540;
+	  int WIDTH;
+	  int HEIGHT;
 	  int totalFrames;
 	  Frame[] frames;
 	  int _currentFrame;
 	  
-	  public Video(String filename) {
+	  public Video(String filename, int width, int height) {
+		  if(width > 0 && height > 0) {
+			  WIDTH = width;
+			  HEIGHT = height;
+		  } else {
+			  WIDTH = 960;
+			  HEIGHT = 540;
+		  }
 		  File inputFile = new File(filename);
 		  long inputLength = inputFile.length();
 		  totalFrames = (int) inputLength/(WIDTH*HEIGHT*3);
 		  frames = new Frame[(int) totalFrames];
-			  for(int frameIndex = 0; frameIndex < totalFrames; frameIndex++){
-				  int index = 3*WIDTH*HEIGHT*frameIndex;
-				  byte[] Frame = ReadFrameFromFile(inputFile, index, 3*HEIGHT*WIDTH);
-				  if (Frame == null)
-					  break;
-				  Frame singleFrame = new Frame(WIDTH, HEIGHT);
-				  for (int i = 0; i < WIDTH*HEIGHT;i++) {
-							byte red = Frame[i];
-							byte green = Frame[i+HEIGHT*WIDTH];
-							byte blue = Frame[i+HEIGHT*WIDTH*2]; 
-							singleFrame.imageBytes[i] = red & 0xff;
-							singleFrame.imageBytes[i+HEIGHT*WIDTH] = green & 0xff;
-							singleFrame.imageBytes[i+HEIGHT*WIDTH*2] = blue & 0xff;
-							index++;
-				  }
-				  frames[frameIndex] = singleFrame;
-			  }
-		  _currentFrame = 0;
+		  
+		  // create an empty frame of size whatever we need with padding
+		  int widthPadding = WIDTH - (WIDTH / MacroBlock.SIZE) * 16;
+		  int heightPadding =HEIGHT - (HEIGHT/ MacroBlock.SIZE * 16);
+		  
+		  int unpaddedWidth = WIDTH;
+		  int unpaddedHeight = HEIGHT;
+		  
+		  WIDTH += widthPadding;
+		  HEIGHT += heightPadding;
+		  
+		  
+		  // read a frame from file including rgb
+		  
+		  for(int frameIndex = 0; frameIndex < totalFrames; frameIndex++){
+			  int index = 3*unpaddedWidth*unpaddedHeight*frameIndex;
+			  byte[] readFrame = ReadFrameFromFile(inputFile, index, 3*unpaddedWidth*unpaddedHeight);
+			  if (readFrame == null)
+				  break;
+			   
+			  Frame paddedFrame = new Frame(WIDTH, HEIGHT, widthPadding, heightPadding);
+			  
+			  for (int i = 0; i < unpaddedWidth*unpaddedHeight;i++) {
+					byte red = readFrame[i];
+					byte green = readFrame[i+unpaddedHeight*unpaddedWidth];
+					byte blue = readFrame[i+unpaddedHeight*unpaddedWidth*2];
+					int pixel = 0x00000000 | ((red & 0xff) << 16) | ((green & 0xff) << 8) | (blue & 0xff);
+					paddedFrame.setPixel(i%WIDTH, i/WIDTH, pixel);
+					index++;
+		  	}
+			  frames[frameIndex] = paddedFrame;
+			  
+		  }
+		  
+		  // copy over the frame into the empty frame
+		  // save the frame
+		  
+//			  for(int frameIndex = 0; frameIndex < totalFrames; frameIndex++){
+//				  int index = 3*WIDTH*HEIGHT*frameIndex;
+//				  byte[] Frame = ReadFrameFromFile(inputFile, index, 3*HEIGHT*WIDTH);
+//				  if (Frame == null)
+//					  break;
+//				  int widthPadding = 16 - (WIDTH % MacroBlock.SIZE);
+//				  int heightPadding = 16 - (HEIGHT % MacroBlock.SIZE);
+//				  Frame singleFrame = new Frame(WIDTH+widthPadding, HEIGHT+heightPadding);
+//				  int widthPaddingLeft = widthPadding/2;
+//				  int widthPaddingRight = widthPadding - widthPaddingLeft;
+//				  int heightPaddingTop = heightPadding/2;
+//				  int heightPaddingBottom = heightPadding - heightPaddingTop;
+//				  int lastPositionTopPadding = 0;
+//				  while(heightPaddingTop != 0) {
+//					  for(int paddedPxls = 0; paddedPxls < singleFrame.width; paddedPxls++){
+//						  singleFrame.imageBytes[lastPositionTopPadding] =0;
+//						  singleFrame.imageBytes[lastPositionTopPadding+singleFrame.height*singleFrame.width] =0;
+//						  singleFrame.imageBytes[lastPositionTopPadding+singleFrame.height*singleFrame.width*2] =0;  
+//						  lastPositionTopPadding++;
+//					  }
+//					  heightPaddingTop--;
+//				  }
+//				  while(widthPaddingLeft !=0) {
+//					  for(int paddedPxls = 0; paddedPxls < singleFrame.width; paddedPxls++){
+//						  singleFrame.imageBytes[lastPositionTopPadding] =0;
+//						  singleFrame.imageBytes[lastPositionTopPadding+singleFrame.height*singleFrame.width] =0;
+//						  singleFrame.imageBytes[lastPositionTopPadding+singleFrame.height*singleFrame.width*2] =0;  
+//						  lastPositionTopPadding++;
+//					  }
+//					  widthPaddingLeft--;
+//				  }
+//				  }
+//				  for (int i = 0; i < WIDTH*HEIGHT;i++) {
+//							byte red = Frame[i];
+//							byte green = Frame[i+HEIGHT*WIDTH];
+//							byte blue = Frame[i+HEIGHT*WIDTH*2]; 
+//							singleFrame.imageBytes[i] = red & 0xff;
+//							singleFrame.imageBytes[i+HEIGHT*WIDTH] = green & 0xff;
+//							singleFrame.imageBytes[i+HEIGHT*WIDTH*2] = blue & 0xff;
+//							index++;
+//				  }
+//				  frames[frameIndex] = singleFrame;
+//			  }
+//		  _currentFrame = 0;
 	  }
 	  
 	  public Frame getFrame(int frameNumber) {
@@ -71,8 +141,11 @@ public class Video {
 			return frames[_currentFrame-1];
 		}
 		public static void main(String[] args) {
+			
 			String testFile = "/Users/student/Desktop/Final/oneperson_960_540.rgb";
-			Video testVideo = new Video(testFile);
+			int width = 960;
+			int height = 540;
+			Video testVideo = new Video(testFile, width, height);
 			System.out.println("Done");
 		}
 
