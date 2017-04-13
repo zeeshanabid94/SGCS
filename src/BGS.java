895c59481905c244dcfc75896391b9a2e9907056
zeeshanabid94 <zeeshanabid94@gmail.com>
2017-04-12 18:02:37 -0700

    Still working on BGS

--- src/BGS.java
+++ src/BGS.java
@@ -1,45 +1,60 @@
 
 public class BGS {
-	Frame _previousFrame;
-	Frame _currentFrame;
+	Video _video;
+	Frame _IFrame;
+	Frame _currentFrame;				// this is current Frame.
 	ArrayList<Vector2D> _motionVectors;
 
 	public BGS() {
-		_previousFrame = NULL;
 		_currentFrame = NULL;
+		_IFrame = NULL;
 		_motionVectors = new ArrayList<>();
 	}
 
-	public void setPreviousFrame(Frame frame) {
-		_previousFrame = frame;
+	public BGS(Video video) {
+		_video = video;
+		_IFrame = _video.getFrame(0);
+		_currentFrame = _video.getFrame(1);
+		_motionVectors = new ArrayList<Vector2D>();
 	}
 
-	public void setCurrentFrame(Frame frame) {
-		_currentFrame = frame;
-	}
+	// public void setPreviousFrame(Frame frame) {
+	// 	_currentFrame = frame;
+	// }
+
+	// public void setCurrentFrame(Frame frame) {
+	// 	_IFrame = frame;
+	// }
 
 	public void CalculateMotionVectors() {
 
-		for (Macroblock block: _previousFrame.getMacroblocks()) {
-			Macroblock match = NULL;
-			int minSad = Integer.MAX_VALUE;
+		for (int i = 1; i < _video.getTotalFrames(); i++) {			// make getTotalFrames method in Video
+			_currentFrame = _video.getFrame(i);
+			Frame PFrame = new Frame(_currentFrame.getWidth(), _currentFrame.getHeight())
 
-			if (match == NULL)
-				match = block;
-
-			for (Macroblock nextBlock : _currentFrame.getMacroblocks()) {
-				int SAD = CalculateSAD(nextBlock, block);
-
-				minSad = Math.Min(SAD,minSad);
-			}
-
-			int motionX = match.getX() - block.getX();
-			int motionY = match.getY() - block.getY();
-
-			Vector2D motionVector = new Vector2D(motionX, motionY);
-
-			_motionVectors.add(motionVector);
 		}
+
+
+		// for (Macroblock block: _currentFrame.getMacroblocks()) {
+		// 	Macroblock match = NULL;
+		// 	int minSad = Integer.MAX_VALUE;
+
+		// 	if (match == NULL)
+		// 		match = block;
+
+		// 	for (Macroblock nextBlock : _IFrame.getMacroblocks()) {
+		// 		int SAD = CalculateSAD(nextBlock, block);
+
+		// 		minSad = Math.Min(SAD,minSad);
+		// 	}
+
+		// 	int motionX = match.getX() - block.getX();
+		// 	int motionY = match.getY() - block.getY();
+
+		// 	Vector2D motionVector = new Vector2D(motionX, motionY);
+
+		// 	_motionVectors.add(motionVector);
+		// }
 	}
 
 	private int CalculateSAD(Macroblock one, Macroblock two) {
